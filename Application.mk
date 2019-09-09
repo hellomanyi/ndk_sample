a481bef2699297b1a3bf09aeea957c2a0fcc0e9b
manyibo <manyibo@bytedance.com>
2019-09-09 21:07:07 +0800

    day1: initialize Android.mk & Application.mk
    Signed-off-by: manyibo <manyibo@bytedance.com>

---
+++ Application.mk
@@ -0,0 +1,26 @@
+# Application Binary Interface
+APP_ABI := armeabi-v7a arm64-v8a
+
+# Select Android.mk
+APP_BUILD_SCRIPT := ./Android.mk
+
+# minSdkVersion
+APP_PLATFORM := android-18
+
+# project paths
+APP_PROJECT_PATH := .
+
+# how to strip libraries
+APP_STRIP_MODE := none
+
+# wrap.sh
+#APP_WRAP_SH
+
+# how to select c++ stdlib
+#APP_STL
+
+# release or debug
+#APP_OPTIM
+
+# select which lib to build
+#APP_MODULES :=
